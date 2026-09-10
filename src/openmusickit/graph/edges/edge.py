d3594b40b062124c026a84776d7737da3e737220
Adam Michael Wood <adam.michael.wood@gmail.com>
2026-09-10 01:10:00 -0700

    edge and next timing

--- src/openmusickit/graph/edges/edge.py
+++ src/openmusickit/graph/edges/edge.py
@@ -2,6 +2,7 @@ from enum import StrEnum, auto
 from dataclasses import dataclass, field
 
 from openmusickit.utils.id import OmkId
+from openmusickit.values.time.duration import Duration
 
 class EdgeType(StrEnum):
     NEXT = auto()
@@ -26,11 +27,58 @@ class EdgeOrigin(StrEnum):
     DERIVED = auto()
     INFERRED = auto()
 
-@dataclass(slots=True)
+@dataclass(slots=True, frozen=True)
 class OmkEdge:
-    type: EdgeType
+    _type: EdgeType
     origin: EdgeOrigin = EdgeOrigin.ASSERTED
     _meta: dict = field(default_factory=dict)
     _id: OmkId = field(default_factory=OmkId.new)
-    
 
+    @property
+    def type(self) -> EdgeType:
+        return self._type
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(type={self.type})"
+
+class TimingAnchor(StrEnum):
+    ONSET = auto()
+    OFFSET = auto()
+
+class NudgeDirection(StrEnum):
+    FORWARD = auto()
+    BACKWARD = auto()
+
+    
+@dataclass(slots=True, kw_only=True)
+class Next(OmkEdge):
+    _type: EdgeType = field(
+        default=EdgeType.NEXT,
+        init=False,
+    )
+    anchor: TimingAnchor = TimingAnchor.OFFSET
+    displacement: Duration | None = None
+
+    @property
+    def type(self) -> EdgeType:
+        return EdgeType.NEXT
+
+    def nudge(self, direction: NudgeDirection, amount: Duration) -> None:
+        """Nudges the displacement of this Next edge in the specified direction by the specified amount."""
+        if type(amount) != type(self.displacement):
+            raise TypeError(
+                f"""Current displacement type ({type(self.displacement)}) does not match nudge amount type ({type(amount)}).
+                Try reconciling duration types with a TemporalRatio."""
+                )
+
+        if self.displacement is None:
+            self.displacement = Duration(0)
+        if direction == NudgeDirection.FORWARD:
+            self.displacement += amount
+        elif direction == NudgeDirection.BACKWARD:
+            self.displacement -= amount
+        else:
+            raise ValueError(f"Invalid nudge direction: {direction}")
+
+    def __repr__(self):
+        return f"Next(anchor={self.anchor}, displacement={self.displacement})"
